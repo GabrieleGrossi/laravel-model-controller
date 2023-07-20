e11ae716e4c6d0a6597e6175fe00ab9d0e9f0625
GabrieleGrossi <grossigabriele98@gmail.com>
2023-07-20 16:24:01 +0200

    create partials

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,14 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="~resources/scss/app.scss">
     <title>@yield('title', 'Laravel')</title>
 </head>
 <body>
-    
+    @include('partials.header')
     <main>
         @yield('main-content')
     </main>
-
+    @include('partials.footer')
     @vite('resources/js/app.js')
 </body>
 </html>
