db4ada5fe96427052d66e36b3296ebbcb23524c0
GabrieleGrossi <125304063+GabrieleGrossi@users.noreply.github.com>
2023-07-20 14:55:52 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+
+import * as bootstrap from 'bootstrap';
+
+//per gli alias, ricordarsi di importarlo qui così: 
+import '~resources/scss/app.scss';
+
+import.meta.glob([
+    '../img/**'
+]);
